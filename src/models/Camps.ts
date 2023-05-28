b71dbeb6993914a71a8c4a48b984428aeaa0d740
Bogdan Balan <bogdanbalan@Bogdans-MacBook-Pro.local>
2023-05-28 20:55:49 +0300

    Fixed Ski section

--- src/models/Camps.ts
+++ src/models/Camps.ts
@@ -125,23 +125,6 @@ export const camps: CampItemType[] = [
 		imageSrc:
 			'https://zeropointstorage.blob.core.windows.net/images/Mask%20group.png',
 	},
-	{
-		id: '9fa464ef-8082-4d80-b561-a7b5b49decef',
-		title: 'Tabara comunicare Apuseni',
-		price: 2100,
-		description:
-			'Tabăra de comunicare dintre părinți și copii este un mediu în care se promovează și se dezvoltă comunicarea sănătoasă și deschisă între cele două generații. Scopul acestei tabere este de a spori înțelegerea reciprocă, de a îmbunătăți relațiile familiale și de a facilita dialogul între părinți și copii.',
-		type: CampType.SKY,
-		startDate: new Date('08/21/2022'),
-		endDate: new Date('08/27/2022'),
-		activity: [
-			'Sesiuni de grup',
-			'Jocuri de rol',
-			'Dezbateri și ateliere interactive',
-		],
-		imageSrc:
-			'https://zeropointstorage.blob.core.windows.net/images/Mask%20group.png',
-	},
 ];
 
 export const heroCampTypes = [
--- src/pages/tabere-ski/index.tsx
+++ src/pages/tabere-ski/index.tsx
@@ -1,16 +1,14 @@
 import ContactInfoSection from '@/components/ContactInfoSection';
 import NoItemSection from '@/components/NoItemSection';
 import { CampType, camps } from '@/models/Camps';
-import { Button, Stack, Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import React from 'react';
-import Image from 'next/image';
-import styles from './../drumetii-mtb/mtb-trips.module.css';
-import Diversity1Icon from '@mui/icons-material/Diversity1';
-import Link from 'next/link';
+import styles from './skyCamp.module.css';
 import CampItem from '@/components/CampItem';
 import Head from 'next/head';
+import CampTypeIcon from '@/components/CampTypeIcon';
 
-const SummerCamp = () => {
+const SkiCamp = () => {
 	const items = camps && camps.filter((camp) => camp.type === CampType.SKY);
 
 	if (items.length === 0) return <NoItemSection />;
@@ -21,8 +19,11 @@ const SummerCamp = () => {
 			</Head>
 			<Stack alignItems={'center'} gap={'100px'}>
 				<div className={styles.heroContainer}>
-					<Diversity1Icon fontSize='large' />
-					<Typography variant='h1'>Drumeții MTB</Typography>
+					<div className={styles.heroImage} />
+					<div className={styles.heroIcon}>
+						<CampTypeIcon type={CampType.SKY} />
+					</div>
+					<Typography variant='h2'>Tabere SKI</Typography>
 				</div>
 				{items.map((item, index) => (
 					<CampItem key={item.id} item={item} isOdd={index % 2 !== 0} />
@@ -33,4 +34,4 @@ const SummerCamp = () => {
 	);
 };
 
-export default SummerCamp;
+export default SkiCamp;
